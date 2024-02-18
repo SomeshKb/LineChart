99050c16a74eede4f2ac4dc91a67aa316da99979
Somesh K Bhaskar <someshkumarbhaskar@gmail.com>
2024-02-18 20:17:30 +0530

    line chart done

---
+++ src/app/line-chart/line-chart.component.ts
@@ -0,0 +1,186 @@
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Inject, PLATFORM_ID, Renderer2, ViewChild } from '@angular/core';
+import { TimeSeries } from '../types';
+import * as data from './../data.json';
+
+@Component({
+  selector: 'app-line-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './line-chart.component.html',
+  styleUrl: './line-chart.component.scss'
+})
+export class LineChartComponent {
+
+  @ViewChild("canvas") canvas!: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D | null = null;
+
+  public lineData: TimeSeries[] =data.data;
+  public isBrowser: boolean;
+  public pointData: any[] = [];
+  hoveredIndex = -1;
+  constructor(@Inject(PLATFORM_ID) platformId: Object, private renderer2: Renderer2) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(data);
+    if (!this.canvas)
+      return;
+
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+
+    this.drawAxis();
+    const { max, min } = this.findMinMax(this.lineData);
+
+    const firstTimestamp = Object.keys(this.lineData[0])[0];
+    const lastTimestamp = Object.keys(this.lineData[this.lineData.length - 1])[0];
+    const timeDifferenceInMilliseconds = this.getTimeDifferenceInMilliseconds(firstTimestamp, lastTimestamp);
+    const timeDifferenceInSeconds = timeDifferenceInMilliseconds / 1000;
+    this.calculateXYCoordinates(550 / timeDifferenceInSeconds, 400 / (max - min), max);
+    this.onMouseMove();
+
+  }
+
+
+  onMouseMove() {
+
+    this.canvas.nativeElement.addEventListener("mousemove", (event) => {
+      const mouseEvent = event as MouseEvent;
+
+      let skipRest = false;
+
+      const mouseX = mouseEvent.clientX - this.canvas.nativeElement.getBoundingClientRect().left;
+      const mouseY = mouseEvent.clientY - this.canvas.nativeElement.getBoundingClientRect().top;
+
+      this.pointData.forEach((point, index) => {
+        if (!this.ctx)
+          return;
+
+        const distance = Math.sqrt((mouseX - point.x) ** 2 + (mouseY - point.y) ** 2);
+
+        if (distance <= 5) {
+          skipRest = true;
+          this.hoveredIndex = index;
+          this.ctx.clearRect(51, 0, 700, 449);
+          this.plotLine();
+          this.plotCircle();
+          setTimeout(() => { }, 1000);
+        }
+      });
+
+      if (skipRest) {
+        return;
+      }
+
+      if (this.hoveredIndex != -1) {
+        if (!this.ctx)
+          return;
+        this.hoveredIndex = -1;
+        this.ctx.clearRect(51, 0, 700, 449);
+        this.plotLine();
+        this.plotCircle();
+      }
+
+
+    });
+  }
+
+  drawAxis() {
+    if (!this.ctx)
+      return;
+    this.ctx.lineWidth = 1;
+    this.ctx.beginPath();
+    this.ctx.moveTo(50, 20);
+    this.ctx.lineTo(50, 450)
+    this.ctx.lineTo(650, 450);
+    this.ctx.stroke();
+  }
+
+  findMinMax(data: TimeSeries[]): { min: number, max: number } {
+    let values: number[] = data.map(entry => Object.values(entry)[0]);
+    let min: number = Math.min(...values);
+    let max: number = Math.max(...values);
+    return { min, max };
+  }
+
+
+  parseTimestamp(timestamp: string): Date {
+    const [datePart, timePart] = timestamp.split(' ');
+    const [day, month, year] = datePart.split('-').map(Number);
+    const [hour, minute, second] = timePart.split(':').map(Number);
+    return new Date(year, month - 1, day, hour, minute, second);
+  }
+
+  getTimeDifferenceInMilliseconds(firstTimestamp: string, lastTimestamp: string): number {
+    // const firstDate = this.parseTimestamp(firstTimestamp);
+    // const lastDate = this.parseTimestamp(lastTimestamp);
+    return Math.abs(parseInt(lastTimestamp) - parseInt(firstTimestamp));
+  }
+
+
+
+
+  plotLine() {
+    if (!this.ctx)
+      return
+
+    this.ctx.beginPath();
+
+    // Draw line
+    this.pointData.map((point, index) => {
+
+      if (!this.ctx)
+        return;
+
+      if (index == 0)
+        this.ctx.moveTo(point.x, point.y);
+
+      this.ctx.lineTo(point.x, point.y);
+
+    });
+    this.ctx.stroke();
+
+
+  }
+
+  plotCircle() {
+    // Draw circle
+    this.pointData.map((point, index) => {
+      if (!this.ctx)
+        return;
+
+      this.ctx.beginPath();
+      if (this.hoveredIndex == index) {
+        this.ctx.arc(point.x, point.y, 4, 0, 2 * Math.PI);
+      } else {
+        this.ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
+      }
+
+      this.ctx.stroke();
+    });
+  }
+
+  calculateXYCoordinates(x_diff: number, y_diff: number, max: number) {
+    const startTime = parseInt(Object.keys(this.lineData[0])[0]); // Update parse Function
+
+    this.lineData.map(x => {
+      const key = parseInt(Object.keys(x)[0]); // Update parse Function
+      const value = Object.values(x)[0];
+      const timeDiff = (key - startTime) / 1000;
+      const valueDiff = max - value;
+      const obj = {
+        x: 50 + 10 + (timeDiff * x_diff),
+        y: 20 + (valueDiff * y_diff),
+        time: key,
+        value: value,
+      }
+      this.pointData.push(obj);
+    });
+
+    this.plotLine();
+    this.plotCircle();
+  }
+
+}
+
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,3 +1,3 @@
 export type TimeSeries = {
-  [key: string]: number;
+  [key: number]: number;
 };
